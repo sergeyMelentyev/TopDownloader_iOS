711f322ebcc3ea540c95c535b4561e940824911f
sergeyMelentyev <sergey.melentyev@gmail.com>
2017-02-11 18:00:53 +0300

    Initial Commit

---
+++ TopDownloader_iOS/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  TopDownloader_iOS
+//
+//  Created by Olga on 11/02/2017.
+//  Copyright © 2017 com.testwebsite. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ TopDownloader_iOS/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TopDownloader_iOS
+//
+//  Created by Olga on 11/02/2017.
+//  Copyright © 2017 com.testwebsite. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
